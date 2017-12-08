eaa5c3e3821a722c380896bb1cedbc6d4e2241fe
Rafal Przywarski <rafal.przywarski@gmail.com>
2017-12-08 21:36:46 +0100

    simple REPL

---
+++ source/repl/main.cpp
@@ -0,0 +1,42 @@
+#include <cleo/reader.hpp>
+#include <cleo/eval.hpp>
+#include <cleo/print.hpp>
+#include <cleo/global.hpp>
+#include <iostream>
+#include <readline/readline.h>
+
+void eval_line(const std::string& line)
+{
+    try
+    {
+        cleo::Root str{cleo::create_string(line)};
+        cleo::Root expr{cleo::read(*str)};
+        cleo::Root result{cleo::eval(*expr)};
+        cleo::Root text{cleo::pr_str(*result)};
+        std::cout << "> " << std::string(cleo::get_string_ptr(*text), cleo::get_string_len(*text)) << std::endl;
+    }
+    catch (cleo::reader_error const& e)
+    {
+        std::cout << "error: " << e.what() << std::endl;
+    }
+    catch (std::exception const& e)
+    {
+        std::cout << "internal error: " << e.what() << std::endl;
+    }
+    catch (...)
+    {
+        std::cout << "unknown internal error" << std::endl;
+    }
+}
+
+int main()
+{
+    char *line;
+
+    while ((line = readline("$ ")) != nullptr)
+    {
+        add_history(line);
+        eval_line(line);
+        std::free(line);
+    }
+}
