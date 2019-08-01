1fec0acc7d6a5a67dd4828d1bdda1e61bb20eade
Rafal Przywarski <rafal.przywarski@gmail.com>
2019-08-01 12:49:58 +0200

    defined UBXI64, BXI64, ADDI64

--- source/core/cleo/vm.hpp
+++ source/core/cleo/vm.hpp
@@ -10,23 +10,34 @@ namespace vm
 using Stack = std::vector<Value>;
 using Byte = char;
 
-constexpr Byte LDC = 1;
-constexpr Byte LDL = 2;
-constexpr Byte LDDV = 3;
-constexpr Byte LDV = 16;
-constexpr Byte LDDF = 15;
-constexpr Byte STL = 4;
-constexpr Byte STVV = 13;
-constexpr Byte STVM = 17;
-constexpr Byte POP = 5;
-constexpr Byte BNIL = 6;
-constexpr Byte BNNIL = 7;
-constexpr Byte BR = 8;
-constexpr Byte CALL = 9;
-constexpr Byte APPLY = 10;
-constexpr Byte THROW = 11;
-constexpr Byte CNIL = 12;
-constexpr Byte IFN = 14;
+constexpr Byte CNIL = 0x00;
+
+constexpr Byte POP = 0x01;
+
+constexpr Byte LDC = 0x10;
+constexpr Byte LDL = 0x11;
+constexpr Byte LDDV = 0x12;
+constexpr Byte LDV = 0x13;
+constexpr Byte LDDF = 0x14;
+
+constexpr Byte STL = 0x20;
+constexpr Byte STVV = 0x21;
+constexpr Byte STVM = 0x22;
+
+constexpr Byte BR = 0x30;
+constexpr Byte BNIL = 0x31;
+constexpr Byte BNNIL = 0x32;
+
+constexpr Byte CALL = 0x40;
+constexpr Byte APPLY = 0x41;
+
+constexpr Byte THROW = 0x48;
+
+constexpr Byte IFN = 0x50;
+
+constexpr Byte UBXI64 = 0x80;
+constexpr Byte BXI64 =  0x81;
+constexpr Byte ADDI64 = 0x82;
 
 void eval_bytecode(Value constants, Value vars, std::uint32_t locals_size, Value exception_table, const Byte *bytecode, std::uint32_t size);
 
