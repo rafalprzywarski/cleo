0be6462af594dd0762d207d3d010c3614f185fa1
Rafal Przywarski <rafal.przywarski@gmail.com>
2019-11-22 01:09:31 +0100

    excluded FastDebug dir from rsync

--- rpi_build_and_test.sh
+++ rpi_build_and_test.sh
@@ -11,7 +11,7 @@ DEST_DIR=/home/pi/remote_builds/cleo
 
 echo "synchronising..."
 ssh ${RPI_ADDRESS} "mkdir -p ${DEST_DIR}"
-rsync -av --delete --exclude xcode --exclude Debug --exclude Release --exclude local_inst * ${RPI_ADDRESS}:${DEST_DIR}/
+rsync -av --delete --exclude xcode --exclude Debug --exclude Release --exclude FastDebug --exclude local_inst * ${RPI_ADDRESS}:${DEST_DIR}/
 
 echo "building..."
 ssh ${RPI_ADDRESS} "cd ${DEST_DIR}; ./build_and_test.sh $@"
